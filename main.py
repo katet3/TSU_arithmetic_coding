6f764f751c4f6da1c333c95410404ff75fa0e67f
Maksim <rikopirdun@gmail.com>
2023-04-02 01:36:03 -0400

    Функция получения файла

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import os
 
 def get_file():
-	pass
+    file = input("input file:\n")
+
+    if os.path.exists(file):
+        return file
+
+    else:
+        exit("The selected file is missing!")
 
 
 def compress():
@@ -11,22 +17,24 @@ def compress():
 def decompress():
 	pass
 
+
 def main():
-	print("|--------------------------------------------------------|")
-	print("|          choose option: [c]ompress/[d]ecompress        |")
-	print("|--------------------------------------------------------|")
+    print("|--------------------------------------------------------|")
+    print("|          choose option: [c]ompress/[d]ecompress        |")
+    print("|--------------------------------------------------------|")
     choose = input()
 
     if choose == 'c':
-		file = get_file()
-		compress(file)
-		
-	elif choose == 'd':
-		file = get_file()
-		decompress(file)
+        file = get_file()
+        compress(file)
+    
+    elif choose == 'd':
+        file = get_file()
+        decompress(file)
 	
     else:
-		exit("wrong option")
+        exit("Unknown choose!")
+		
 
 if __name__ == "__main__":
 	main()
